8dccf46da538e3410feb7e215b61f308bc14575d
ygallaw <112808872+ygallaw@users.noreply.github.com>
2022-11-06 18:44:10 +0200

    Refactor

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 function lambda_function_builder { 
     mkdir -p ./results
-    result_dir = $(realpath ./results)
+    results_dir = $(realpath ./results)
     pushd ./src/
     for f in *;  do 
         if [[ "$f" != "helper" && "$f" != "lambda-extension" && -d $f ]]; 
@@ -9,7 +9,7 @@ function lambda_function_builder {
             pushd "$f";
             sam build 
             cd .aws-sam/build/LambdaFunction/
-            zip -r ../../../../../results/"$f" .  
+            zip -r "$results_dir/$f" .  
             echo "Doing something in folder `pwd`/$f"; 
             popd; 
         fi;  
